59d33afd2cccfc5b27c2df01af5bd429a21704c5
dev <devpalwar06@gmail.com>
2023-08-29 18:30:09 +0530

    CORS configuration

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const connectDatbase = require("./Database/config");
+const connectDatabase = require("./Database/config");
 const urlRouter = require("./Routes/url");
 require("dotenv").config();
 
 const app = express();
-connectDatbase();
+connectDatabase();
 
-app.get('/', (req, res)=> res.send("Nice working"))
+// Add CORS configuration middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.get('/', (req, res) => res.send("Nice working"))
 app.use(express.json());
 app.use('/url', urlRouter);
 
-app.listen(9000, ()=>{
-    console.log("Server running smooth on port 9000");
-})
+app.listen(9000, () => {
+  console.log("Server running smooth on port 9000");
+});
